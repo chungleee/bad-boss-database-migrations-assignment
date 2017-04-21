1d03949e0b687912abf854c62e9d4df1fdd9a899
Leon <leon.karho.chung@gmail.com>
2017-04-21 12:07:26 -0400

    generate Locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421155607) do
+ActiveRecord::Schema.define(version: 20170421160405) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
